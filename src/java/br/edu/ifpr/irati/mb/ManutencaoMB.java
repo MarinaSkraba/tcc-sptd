962a6fd559c7ca271ce6e23cf027da59092cba64
Marina <Marina@casa>
2018-08-10 11:02:37 -0300

    10/08/2018 - Marina

--- src/java/br/edu/ifpr/irati/mb/ManutencaoMB.java
+++ src/java/br/edu/ifpr/irati/mb/ManutencaoMB.java
@@ -7,7 +7,9 @@ package br.edu.ifpr.irati.mb;
 
 import br.edu.ifpr.irati.dao.Dao;
 import br.edu.ifpr.irati.dao.GenericDAO;
+import br.edu.ifpr.irati.modelo.Horario;
 import br.edu.ifpr.irati.modelo.ManutencaoEnsino;
+import br.edu.ifpr.irati.modelo.TipoManutencao;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 
@@ -16,16 +18,27 @@ public class ManutencaoMB {
 
     private ManutencaoEnsino manutencaoEnsino;
     private List<ManutencaoEnsino> manutencoesEnsino;
+    private Horario horario;
+    private TipoManutencao tipoManutencao;
 
     public ManutencaoMB() {
 
+        horario = new Horario();
+        tipoManutencao = new TipoManutencao();
         manutencaoEnsino = new ManutencaoEnsino();
         Dao<ManutencaoEnsino> manutencaoEnsinoDAO = new GenericDAO<>(ManutencaoEnsino.class);
         manutencoesEnsino = manutencaoEnsinoDAO.buscarTodos(ManutencaoEnsino.class);
     }
 
     public void salvar() {
+
         Dao<ManutencaoEnsino> manutencaoEnsinoDAO = new GenericDAO<>(ManutencaoEnsino.class);
+        Dao<TipoManutencao> tipoManutencaoDAO = new GenericDAO<>(TipoManutencao.class);
+        tipoManutencaoDAO.salvar(tipoManutencao);
+        Dao<Horario> horarioDAO = new GenericDAO<>(Horario.class);
+        horarioDAO.salvar(horario);  
+        manutencaoEnsino.setTipoManutencao(tipoManutencao);
+        manutencaoEnsino.getHorarios().add(horario);
         manutencaoEnsinoDAO.salvar(manutencaoEnsino);
         manutencaoEnsino = new ManutencaoEnsino();
         manutencoesEnsino = manutencaoEnsinoDAO.buscarTodos(ManutencaoEnsino.class);
@@ -46,5 +59,21 @@ public class ManutencaoMB {
 
     public void setManutencoesEnsino(List<ManutencaoEnsino> manutencoesEnsino) {
         this.manutencoesEnsino = manutencoesEnsino;
+    }
+
+    public TipoManutencao getTipoManutencao() {
+        return tipoManutencao;
+    }
+
+    public void setTipoManutencao(TipoManutencao tipoManutencao) {
+        this.tipoManutencao = tipoManutencao;
+    }
+
+    public Horario getHorario() {
+        return horario;
+    }
+
+    public void setHorario(Horario horario) {
+        this.horario = horario;
     }
 }
