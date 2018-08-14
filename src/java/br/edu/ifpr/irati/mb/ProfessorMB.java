a3413f9684592d1602b3d4adf17692a570ea1472
Marina <Marina@casa>
2018-08-14 09:13:52 -0300

    14/08/2018 - Marina

---
+++ src/java/br/edu/ifpr/irati/mb/ProfessorMB.java
@@ -0,0 +1,37 @@
+package br.edu.ifpr.irati.mb;
+
+import br.edu.ifpr.irati.modelo.Professor;
+import java.util.ArrayList;
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class ProfessorMB {
+
+    private Professor professor;
+    private List<Professor> professores;
+
+    public ProfessorMB() {
+
+        professor = new Professor();
+        professores = new ArrayList();
+
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public List<Professor> getProfessores() {
+        return professores;
+    }
+
+    public void setProfessores(List<Professor> professores) {
+        this.professores = professores;
+    }
+
+}
