a39e203619e8625861b38e84e987397fcb5db946
willi <willi@10.33.0.83>
2018-08-27 21:46:43 -0300

    magnabosco - 27/08/2018

--- src/java/br/edu/ifpr/irati/mb/AulaMB.java
+++ src/java/br/edu/ifpr/irati/mb/AulaMB.java
@@ -14,8 +14,10 @@ import br.edu.ifpr.irati.modelo.TipoOferta;
 import java.util.ArrayList;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 
 @ManagedBean
+@SessionScoped
 public class AulaMB {
 
     private Aula aula;
