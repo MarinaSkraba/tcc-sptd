45af36ed3d4394151760daa45b5ed8d169d8ba25
Marina <Marina@marina>
2018-08-30 09:25:48 -0300

    30/08/2018 - Marina

--- src/java/br/edu/ifpr/irati/dao/AulaDAO.java
+++ src/java/br/edu/ifpr/irati/dao/AulaDAO.java
@@ -2,6 +2,7 @@ package br.edu.ifpr.irati.dao;
 
 import br.edu.ifpr.irati.modelo.Aula;
 import br.edu.ifpr.irati.modelo.Professor;
+import br.edu.ifpr.irati.modelo.Usuario;
 import br.edu.ifpr.irati.util.HibernateUtil;
 import java.io.Serializable;
 import java.util.List;
@@ -15,6 +16,7 @@ public class AulaDAO implements IAulaDao {
         Session session = HibernateUtil.getSessionFactory().openSession();
         String hql = "from aula, professor where estadoAula = 'Ativo' and idUsuario = ? ";
         Query query = session.createQuery(hql);
+        query.setSerializable(0, idUsuario);
         List results = query.list();
         session.clear();
         session.close();
