830b7e709c0239abbdf601d98cdc3e38e0af7d8f
willi <willi@MAGNABOSCO-WILL>
2018-09-04 11:46:30 -0300

    magnabosco - 04/09/2018

--- src/java/br/edu/ifpr/irati/mb/PTDMB.java
+++ src/java/br/edu/ifpr/irati/mb/PTDMB.java
@@ -39,6 +39,7 @@ import javax.faces.bean.ManagedBean;
 public class PTDMB {
 
     private PTD ptd;
+    private List<PTD> pdtsEmAvaliacao;
     private Administracao administracao;
     private List<Administracao> administracoes;
     private Apoio apoio;
@@ -61,6 +62,7 @@ public class PTDMB {
     public PTDMB() {
 
         ptd = new PTD();
+        pdtsEmAvaliacao = new ArrayList<>();
         administracao = new Administracao();
         apoio = new Apoio();
         atividadeASerProposta = new AtividadeASerProposta();
@@ -317,5 +319,19 @@ public class PTDMB {
 
     public void setProjetosPesquisa(List<ProjetoPesquisa> projetosPesquisa) {
         this.projetosPesquisa = projetosPesquisa;
+    }
+
+    /**
+     * @return the pdtsEmAvaliacao
+     */
+    public List<PTD> getPdtsEmAvaliacao() {
+        return pdtsEmAvaliacao;
+    }
+
+    /**
+     * @param pdtsEmAvaliacao the pdtsEmAvaliacao to set
+     */
+    public void setPdtsEmAvaliacao(List<PTD> pdtsEmAvaliacao) {
+        this.pdtsEmAvaliacao = pdtsEmAvaliacao;
     }
 }
