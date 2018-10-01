1ed85398d367c4bdcd35ad5d53268cc1c24dc899
Marina <Marina@marina>
2018-10-01 18:15:48 -0300

    01/10/18-  Marina

--- src/java/br/edu/ifpr/irati/modelo/Participacao.java
+++ src/java/br/edu/ifpr/irati/modelo/Participacao.java
@@ -1,6 +1,8 @@
 package br.edu.ifpr.irati.modelo;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity(name = "participacao")
 public class Participacao implements Serializable {
@@ -26,11 +29,20 @@ public class Participacao implements Serializable {
     @JoinColumn(name = "professor_idUsuario")
     private Professor professor;
 
+    @ManyToOne
+    @JoinColumn(name = "projetopesquisaextensao_idProjetoPesquisaExtensao")
+    private List<ProjetoPesquisaExtensao> projetosPesquisaExtensao;
+
+    @OneToMany
+    private List<Horario> horariosParticipacao;
+
     public Participacao() {
 
         this.idParticipacao = 0;
         this.estadoParticipacao = "";
         this.professor = new Professor();
+        this.horariosParticipacao = new ArrayList();
+        this.projetosPesquisaExtensao = new ArrayList();
 
     }
 
@@ -39,13 +51,21 @@ public class Participacao implements Serializable {
         this.rotulo = rotulo;
         this.estadoParticipacao = estadoParticipacao;
         this.professor = new Professor();
+        this.horariosParticipacao = new ArrayList();
+        this.projetosPesquisaExtensao = new ArrayList();
     }
 
-    public Participacao(int idParticipacao, String rotulo, String estadoParticipacao, Professor professor) {
+    public Participacao(int idParticipacao, String rotulo, String estadoParticipacao, Professor professor, List<ProjetoPesquisaExtensao> projetosPesquisaExtensao, List<Horario> horariosParticipacao) {
         this.idParticipacao = idParticipacao;
         this.rotulo = rotulo;
         this.estadoParticipacao = estadoParticipacao;
         this.professor = professor;
+        this.projetosPesquisaExtensao = projetosPesquisaExtensao;
+        this.horariosParticipacao = horariosParticipacao;
+    }
+
+    public void adicionarHorario(Horario horario) {
+        this.getHorariosParticipacao().add(horario);
     }
 
     public int getIdParticipacao() {
@@ -78,6 +98,22 @@ public class Participacao implements Serializable {
 
     public void setEstadoParticipacao(String estadoParticipacao) {
         this.estadoParticipacao = estadoParticipacao;
+    }
+
+    public List<Horario> getHorariosParticipacao() {
+        return horariosParticipacao;
+    }
+
+    public void setHorariosParticipacao(List<Horario> horariosParticipacao) {
+        this.horariosParticipacao = horariosParticipacao;
+    }
+
+    public List<ProjetoPesquisaExtensao> getProjetosPesquisaExtensao() {
+        return projetosPesquisaExtensao;
+    }
+
+    public void setProjetosPesquisaExtensao(List<ProjetoPesquisaExtensao> projetosPesquisaExtensao) {
+        this.projetosPesquisaExtensao = projetosPesquisaExtensao;
     }
 
 }
