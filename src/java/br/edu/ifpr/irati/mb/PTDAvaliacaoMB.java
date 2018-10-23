e3bba62f0b33eda291a9a855a94602f54b6450f7
willi <willi@10.33.0.83>
2018-10-23 12:03:59 -0200

    magnabosco 23-10-2018

--- src/java/br/edu/ifpr/irati/mb/PTDAvaliacaoMB.java
+++ src/java/br/edu/ifpr/irati/mb/PTDAvaliacaoMB.java
@@ -4,9 +4,13 @@ import br.edu.ifpr.irati.dao.Dao;
 import br.edu.ifpr.irati.dao.GenericDAO;
 import br.edu.ifpr.irati.modelo.Administracao;
 import br.edu.ifpr.irati.modelo.Apoio;
+import br.edu.ifpr.irati.modelo.Aula;
 import br.edu.ifpr.irati.modelo.DiretorEnsino;
+import br.edu.ifpr.irati.modelo.ManutencaoEnsino;
+import br.edu.ifpr.irati.modelo.OutroTipoAtividade;
 import br.edu.ifpr.irati.modelo.PTD;
 import br.edu.ifpr.irati.modelo.Participacao;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
@@ -23,6 +27,9 @@ public class PTDAvaliacaoMB {
 
     public PTDAvaliacaoMB() {
         ptdEmAvaliacao = new PTD();
+        irregularidadesPTDAvaliacao = new ArrayList<>();
+        participacoesAutorPTDAvaliacao = new ArrayList<>();
+        participacoesColabPTDAvaliacao = new ArrayList<>();
     }
 
     public String abrirPTDEmAvaliacao(PTD ptd) {
@@ -31,9 +38,18 @@ public class PTDAvaliacaoMB {
         return "PTDEmAvaliacao";
     }
     
+    public int verificarConteúdoListaParaOpacidade(List<String> lista) {
+        if (lista.isEmpty()) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
     public void verificarCargaHorariaPTDEdicao() {
 
         PTD ptd = new PTD();
+        double cargaHorariaTotalPTDAux = 0;
 
         for (Administracao adm : getPtdEmAvaliacao().getAdministrativas()) {
             ptd.setCargaHorariaSecaoAdministracao(ptd.getCargaHorariaSecaoAdministracao()
@@ -74,32 +90,32 @@ public class PTDAvaliacaoMB {
             if (ptd.getCargaHorariaSecaoApoioEnsino() > 4 && (ptd.getCargaHorariaSecaoProjetoPesquisaExtensaoAutor() + ptd.getCargaHorariaSecaoProjetoPesquisaExtensaoColab()) != 0) {
                 irregularidadesPTDAvaliacao.add("A carga horária é superior à 4 horas em Apoio ao Ensino!");
             } else if (ptd.getCargaHorariaSecaoApoioEnsino() < 4) {
-                getIrregularidadesPTDEdicao().add("A carga horária é  inferior à 4 horas em Apoio ao Ensino!");
+                irregularidadesPTDAvaliacao.add("A carga horária é  inferior à 4 horas em Apoio ao Ensino!");
             }
 
         }
 
-        for (Aula a : getPtd().getAulas()) {
+        for (Aula a : getPtdEmAvaliacao().getAulas()) {
             ptd.setCargaHorariaSecaoAulas(ptd.getCargaHorariaSecaoAulas() + a.getCargaHorariaTotal());
 
         }
 
-        if (getPtd().getProfessor().getRegimeTrabalho().equals("20h")) {
+        if (getPtdEmAvaliacao().getProfessor().getRegimeTrabalho().equals("20h")) {
 
             if (ptd.getCargaHorariaSecaoAulas() < 8) {
 
-                getIrregularidadesPTDEdicao().add("A carga horária é inferior à 8 horas em Aula!");
+                irregularidadesPTDAvaliacao.add("A carga horária é inferior à 8 horas em Aula!");
 
             } else if (ptd.getCargaHorariaSecaoAulas() > 12) {
 
-                getIrregularidadesPTDEdicao().add("A carga horária é superior à 12 horas em Aula!");
+                irregularidadesPTDAvaliacao.add("A carga horária é superior à 12 horas em Aula!");
 
             }
 
-        } else if (getPtd().getProfessor().getRegimeTrabalho().equalsIgnoreCase("40h") | getPtd().getProfessor().getRegimeTrabalho().equalsIgnoreCase("Dedicação Exclusiva")) {
+        } else if (getPtdEmAvaliacao().getProfessor().getRegimeTrabalho().equalsIgnoreCase("40h") | getPtdEmAvaliacao().getProfessor().getRegimeTrabalho().equalsIgnoreCase("Dedicação Exclusiva")) {
             if (ptd.getCargaHorariaSecaoAulas() < 12) {
 
-                getIrregularidadesPTDEdicao().add("A carga horária é inferior à 12 horas em Aula!");
+                irregularidadesPTDAvaliacao.add("A carga horária é inferior à 12 horas em Aula!");
 
             } else if (ptd.getCargaHorariaSecaoAulas() > 16) {
 
@@ -110,14 +126,14 @@ public class PTDAvaliacaoMB {
 
                     if ((ptd.getCargaHorariaSecaoAulas() - excessoEsperadoAula) > 16) {
 
-                        getIrregularidadesPTDEdicao().add("Mesmo descontando a carga horária redistribuída de projeto de pesquisa"
+                        irregularidadesPTDAvaliacao.add("Mesmo descontando a carga horária redistribuída de projeto de pesquisa"
                                 + "e/ou extensão para aula e apoio ao ensino, o componente aula apresenta carga horária"
                                 + "superior à 16 horas");
 
                     }
 
                 } else {
-                    getIrregularidadesPTDEdicao().add("A carga horária é superior à 16 horas em Aula!");
+                    irregularidadesPTDAvaliacao.add("A carga horária é superior à 16 horas em Aula!");
                 }
 
                 if (ptd.getCargaHorariaSecaoAulas() >= 12 && ptd.getCargaHorariaSecaoAulas() <= 16) {
@@ -128,7 +144,7 @@ public class PTDAvaliacaoMB {
 
                         if ((ptd.getCargaHorariaSecaoAulas() - excessoEsperadoAula) < 12) {
 
-                            getIrregularidadesPTDEdicao().add("Descontando a carga horária redistribuída de projeto de pesquisa"
+                            irregularidadesPTDAvaliacao.add("Descontando a carga horária redistribuída de projeto de pesquisa"
                                     + "e/ou extensão para aula e apoio ao ensino, o componente aula apresenta carga horária"
                                     + "inferior à 12 horas");
 
@@ -137,52 +153,52 @@ public class PTDAvaliacaoMB {
                 }
             }
         }
-        for (ManutencaoEnsino me : getPtd().getManutencoesEnsino()) {
+        for (ManutencaoEnsino me : getPtdEmAvaliacao().getManutencoesEnsino()) {
             ptd.setCargaHorariaSecaoManutencaoEnsino(ptd.getCargaHorariaSecaoManutencaoEnsino() + me.getCargaHorariaSemanalManutencaoEnsino());
         }
 
         if (ptd.getCargaHorariaSecaoManutencaoEnsino() != 4) {
             if (ptd.getCargaHorariaSecaoManutencaoEnsino() < 4) {
 
-                getIrregularidadesPTDEdicao().add("A carga horária é inferior"
+                irregularidadesPTDAvaliacao.add("A carga horária é inferior"
                         + " à 4 horas em Manutenção ao Ensino");
 
             } else if (ptd.getCargaHorariaSecaoManutencaoEnsino() > 4) {
 
-                getIrregularidadesPTDEdicao().add("A carga horária é superior "
+                irregularidadesPTDAvaliacao.add("A carga horária é superior "
                         + "à 4 horas em Manutenção ao Ensino!");
 
             }
         }
 
-        for (OutroTipoAtividade ota : getPtd().getOutrosTiposAtividades()) {
+        for (OutroTipoAtividade ota : getPtdEmAvaliacao().getOutrosTiposAtividades()) {
             ptd.setCargaHorariaSecaoOutroTipoAtividade(ptd.getCargaHorariaSecaoOutroTipoAtividade() + ota.getCargaHorariaSemanalOutroTipoAtividade());
         }
-        if (!getPtd().getApoios().isEmpty() && !getPtd().getAulas().isEmpty() && !getPtd().getManutencoesEnsino().isEmpty()) {
-            if (getPtd().getProfessor().getRegimeTrabalho().equalsIgnoreCase("20h")) {
+        if (!getPtdEmAvaliacao().getApoios().isEmpty() && !getPtdEmAvaliacao().getAulas().isEmpty() && !getPtdEmAvaliacao().getManutencoesEnsino().isEmpty()) {
+            if (getPtdEmAvaliacao().getProfessor().getRegimeTrabalho().equalsIgnoreCase("20h")) {
                 if ((ptd.getCargaHorariaSecaoApoioEnsino() + ptd.getCargaHorariaSecaoAulas() + ptd.getCargaHorariaSecaoManutencaoEnsino()) < 8) {
 
-                    irregularidadesPTDEdicao.add("A carga horária dedicada a Atividades de Ensino(apoio,manutenção e aulas) é inferior à 8 horas");
+                    irregularidadesPTDAvaliacao.add("A carga horária dedicada a Atividades de Ensino(apoio,manutenção e aulas) é inferior à 8 horas");
 
                 } else if ((ptd.getCargaHorariaSecaoApoioEnsino() + ptd.getCargaHorariaSecaoAulas() + ptd.getCargaHorariaSecaoManutencaoEnsino()) > 20) {
 
-                    irregularidadesPTDEdicao.add("A carga horária dedicada a Atividades de Ensino(apoio,manutenção e aulas) é superior à 12 horas");
+                    irregularidadesPTDAvaliacao.add("A carga horária dedicada a Atividades de Ensino(apoio,manutenção e aulas) é superior à 12 horas");
 
                 }
             }
-            if (getPtd().getProfessor().getRegimeTrabalho().equalsIgnoreCase("40h") | getPtd().getProfessor().getRegimeTrabalho().equalsIgnoreCase("Dedicação Exclusiva")) {
+            if (getPtdEmAvaliacao().getProfessor().getRegimeTrabalho().equalsIgnoreCase("40h") | getPtdEmAvaliacao().getProfessor().getRegimeTrabalho().equalsIgnoreCase("Dedicação Exclusiva")) {
 
                 if ((ptd.getCargaHorariaSecaoApoioEnsino()
                         + ptd.getCargaHorariaSecaoAulas()
                         + ptd.getCargaHorariaSecaoManutencaoEnsino()) < 12) {
 
-                    irregularidadesPTDEdicao.add("A carga horária dedicada a Atividades de Ensino(apoio,manutenção e aulas) é inferior à 12 horas");
+                    irregularidadesPTDAvaliacao.add("A carga horária dedicada a Atividades de Ensino(apoio,manutenção e aulas) é inferior à 12 horas");
 
                 } else if ((ptd.getCargaHorariaSecaoApoioEnsino()
                         + ptd.getCargaHorariaSecaoAulas()
                         + ptd.getCargaHorariaSecaoManutencaoEnsino()) > 24) {
 
-                    irregularidadesPTDEdicao.add("A carga horária dedicada a Atividades de Ensino(apoio,manutenção e aulas) é superior à 24 horas");
+                    irregularidadesPTDAvaliacao.add("A carga horária dedicada a Atividades de Ensino(apoio,manutenção e aulas) é superior à 24 horas");
 
                 }
             }
@@ -195,30 +211,28 @@ public class PTDAvaliacaoMB {
                 + ptd.getCargaHorariaSecaoProjetoPesquisaExtensaoAutor()
                 + ptd.getCargaHorariaSecaoProjetoPesquisaExtensaoColab();
         double regime = 20;
-        if ((getPtd().getProfessor().getRegimeTrabalho().equals("40h") | getPtd().getProfessor().getRegimeTrabalho().equals("Dedicação Exclusiva")) && cargaHorariaTotalPTDAux != 40) {
+        if ((getPtdEmAvaliacao().getProfessor().getRegimeTrabalho().equals("40h") | getPtdEmAvaliacao().getProfessor().getRegimeTrabalho().equals("Dedicação Exclusiva")) && cargaHorariaTotalPTDAux != 40) {
 
-            irregularidadesPTDEdicao.add("A carga horária do PTD diverge com seu regime de trabalho de 40h");
+            irregularidadesPTDAvaliacao.add("A carga horária do PTD diverge com seu regime de trabalho de 40h");
 
-        } else if (getPtd().getProfessor().getRegimeTrabalho().equals("20h") && cargaHorariaTotalPTDAux != 20) {
+        } else if (getPtdEmAvaliacao().getProfessor().getRegimeTrabalho().equals("20h") && cargaHorariaTotalPTDAux != 20) {
 
-            irregularidadesPTDEdicao.add("A carga horária do PTD diverge com seu regime de trabalho de 20h");
+            irregularidadesPTDAvaliacao.add("A carga horária do PTD diverge com seu regime de trabalho de 20h");
 
         }
-        if (getPtd().getProfessor().getRegimeTrabalho().equalsIgnoreCase("40h") | getPtd().getProfessor().getRegimeTrabalho().equalsIgnoreCase("Dedicação Exclusiva")) {
+        if (getPtdEmAvaliacao().getProfessor().getRegimeTrabalho().equalsIgnoreCase("40h") | getPtdEmAvaliacao().getProfessor().getRegimeTrabalho().equalsIgnoreCase("Dedicação Exclusiva")) {
             regime = 40;
         }
-        if (regime == cargaHorariaTotalPTDAux) {
-            setEstadoCargaHorariaPTD("CORRETO");
-        } else {
-            setEstadoCargaHorariaPTD("INCORRETO");
+
+    }
+
+    public String verificacaoIrregularidadesNotificacoesDiretorEnsino(PTD ptd) {
+        String resposta = "Correto";
+        verificarCargaHorariaPTDEdicao();
+        if (!irregularidadesPTDAvaliacao.isEmpty()) {
+            resposta = "Irregular";
         }
-
-        for (String irregularidade : irregularidadesPTDEdicao) {
-            getIrregularidadesPTDAvaliacao().add(irregularidade);
-        }
-
-        cargaHorariaTotalPTDPTDEdicao = cargaHorariaTotalPTDAux;
-
+        return resposta;
     }
 
     public String verificarPossibilidadeReprovacao() {
@@ -254,6 +268,18 @@ public class PTDAvaliacaoMB {
         getPtdEmAvaliacao().setDiretorEnsino(diretorEnsino);
         ptdDAOGenerico.alterar(getPtdEmAvaliacao());
     }
+    
+    public void atualizarListasParticipacoesPTDAvaliacao() {
+        participacoesAutorPTDAvaliacao = new ArrayList<>();
+        participacoesColabPTDAvaliacao = new ArrayList<>();
+        for (Participacao part : ptdEmAvaliacao.getParticipacoes()) {
+            if (part.getRotulo().equalsIgnoreCase("Autor")) {
+                participacoesAutorPTDAvaliacao.add(part);
+            } else {
+                participacoesColabPTDAvaliacao.add(part);
+            }
+        }
+    }
 
     /**
      * @return the ptdEmAvaliacao
@@ -273,11 +299,13 @@ public class PTDAvaliacaoMB {
      * @return the participacoesAutorPTDAvaliacao
      */
     public List<Participacao> getParticipacoesAutorPTDAvaliacao() {
+        atualizarListasParticipacoesPTDAvaliacao();
         return participacoesAutorPTDAvaliacao;
     }
 
     /**
-     * @param participacoesAutorPTDAvaliacao the participacoesAutorPTDAvaliacao to set
+     * @param participacoesAutorPTDAvaliacao the participacoesAutorPTDAvaliacao
+     * to set
      */
     public void setParticipacoesAutorPTDAvaliacao(List<Participacao> participacoesAutorPTDAvaliacao) {
         this.participacoesAutorPTDAvaliacao = participacoesAutorPTDAvaliacao;
@@ -287,11 +315,13 @@ public class PTDAvaliacaoMB {
      * @return the participacoesColabPTDAvaliacao
      */
     public List<Participacao> getParticipacoesColabPTDAvaliacao() {
+        atualizarListasParticipacoesPTDAvaliacao();
         return participacoesColabPTDAvaliacao;
     }
 
     /**
-     * @param participacoesColabPTDAvaliacao the participacoesColabPTDAvaliacao to set
+     * @param participacoesColabPTDAvaliacao the participacoesColabPTDAvaliacao
+     * to set
      */
     public void setParticipacoesColabPTDAvaliacao(List<Participacao> participacoesColabPTDAvaliacao) {
         this.participacoesColabPTDAvaliacao = participacoesColabPTDAvaliacao;
